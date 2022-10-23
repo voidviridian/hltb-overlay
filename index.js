08fb7b741200c44546112ccaf1ae949905c1a16d
voidviridian <116373739+voidviridian@users.noreply.github.com>
2022-10-23 20:55:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,36 @@ const handler = require('serve-handler');
 let hltb = require('howlongtobeat');
 let hltbService = new hltb.HowLongToBeatService();
 
+const rewriteData = ({ name, platforms, imageUrl }) => {
+  const nintendo = platforms
+    .filter((platform) => platform.includes('Nintendo'))
+    .map((platform) => platform.replace(/^Nintendo /, ''));
+
+  const sony = ['PlayStation ' + platforms
+    .filter((platform) => platform.includes('PlayStation'))
+    .map((platform) => platform.replace(/^PlayStation /, '')).join('|')];
+  
+  const xbox = ['Xbox ' + platforms
+    .filter((platform) => platform.includes('Xbox'))
+    .map((platform) => platform.replace(/^Xbox /, ''))
+    .map((platform) => platform.replace('X/S', 'X|S'))
+    .join('/')];
+  
+  platforms = platforms
+    .filter((platform) => !platform.includes('Nintendo'))
+    .filter((platform) => !platform.includes('PlayStation'))
+    .filter((platform) => !platform.includes('Xbox'));
+
+  platforms = [...platforms, ...nintendo, ...sony, ...xbox];
+  return { name, platforms, imageUrl };
+};
+
 module.exports = async (req, res) => {
   const { url } = req;
   if (url.includes('/game/')) {
     const gameName = decodeURI(url.replace('/game/', ''));
     const [data] = await hltbService.search(gameName);
-    res.end(JSON.stringify(data));
+    res.end(JSON.stringify(rewriteData(data)));
   } else {
     await handler(req, res, {
       directoryListing: false,
